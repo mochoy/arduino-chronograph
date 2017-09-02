701300a80d495213ea38fb9831db3bcce314b4bc
BobLakensi <montgomery.okdaily@gmail.com>
2017-09-02 14:26:27 -0700

    calculate chrono readings based on time, commit before new lib integration

--- arduino-chronograph.ino
+++ arduino-chronograph.ino
@@ -1,14 +1,23 @@
-#include "U8glib.h"
+#include <Arduino.h>
+#include <U8g2lib.h>
 
-#define IR_REC_ONE_PIN 0;
-#define IR_REC_TWO_PIN 1;
-#define GATE_DISPLACEMENT 4;    //in inches
-#define IR_GATE_TRIP_VAL 95;
+#ifdef U8X8_HAVE_HW_SPI
+#include <SPI.h>
+#endif
+#ifdef U8X8_HAVE_HW_I2C
+#include <Wire.h>
+#endif
+
+#define IR_REC_ONE_PIN 0
+#define IR_REC_TWO_PIN 1
+#define GATE_DISPLACEMENT 4    //in inches
+#define IR_GATE_TRIP_VAL 95
 
 U8GLIB_SSD1306_128X64 u8g(U8G_I2C_OPT_NONE);   
 
 double firstTripTime, secondTripTime;
 boolean hasFirstTripped = false;
+String chronoValsToPrint = "";
 
 void setup () {
   setupDisplay();
@@ -43,23 +52,26 @@ void loopDisplay () {
 
 void draw () {
   u8g.setFont(u8g_font_unifont);
-  u8g.drawStr( 0, 22, "Hello Worlds!");
+  u8g.drawStr( 0, 22, chronoValsToPrint);
 }
 
 void chrono () {
-    if (hasFirstTripped == false && map(analogRead(IR_REC_ONE_PIN), 0, 1024, 0, 100) > IR_GATE_TRIP_VAL)  {
+    if (hasFirstTripped == false && (map(analogRead(IR_REC_ONE_PIN), 0, 1024, 0, 100) > IR_GATE_TRIP_VAL) ) {
         firstTripTime = micros();
         hasFirstTripped = true;
     }
     if (hasFirstTripped == true && (map(analogRead(IR_REC_TWO_PIN), 0, 1024, 0, 100) > IR_GATE_TRIP_VAL) ) {
         secondTripTime = micros();
         hasFirstTripped = false;
-        calculateChronoReadings();
+        chronoValsToPrint = (String)calculateChronoReadings();
     }
 }
 
 double calculateChronoReadings () {
-    
+    double timeElapsed = (secondTripTime - firstTripTime) * 1000000;
+    double displacement = GATE_DISPLACEMENT / 12;
+
+    return displacement/timeElapsed;
 }
 
 
