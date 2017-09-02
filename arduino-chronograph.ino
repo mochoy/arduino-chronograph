600daf45bd296851674a4187f8809ce553d50ddf
BobLakensi <montgomery.okdaily@gmail.com>
2017-09-02 14:37:31 -0700

    intgrate new lib U8g2 to print hello world

--- arduino-chronograph.ino
+++ arduino-chronograph.ino
@@ -13,65 +13,48 @@
 #define GATE_DISPLACEMENT 4    //in inches
 #define IR_GATE_TRIP_VAL 95
 
-U8GLIB_SSD1306_128X64 u8g(U8G_I2C_OPT_NONE);   
+U8G2_SSD1306_128X64_NONAME_F_SW_I2C u8g2(U8G2_R0, /* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ U8X8_PIN_NONE);
 
 double firstTripTime, secondTripTime;
 boolean hasFirstTripped = false;
 String chronoValsToPrint = "";
 
 void setup () {
-  setupDisplay();
+  u8g2.begin();
 }
 
 void loop () {
-  loopDisplay();
   chrono();
+  display();
 }
 
-void setupDisplay () {
-  if ( u8g.getMode() == U8G_MODE_R3G3B2 ) {
-    u8g.setColorIndex(255);     
-  }
-  else if ( u8g.getMode() == U8G_MODE_GRAY2BIT ) {
-    u8g.setColorIndex(3);    
-  }
-  else if ( u8g.getMode() == U8G_MODE_BW ) {
-    u8g.setColorIndex(1);  
-  }
-  else if ( u8g.getMode() == U8G_MODE_HICOLOR ) {
-    u8g.setHiColorByRGB(255,255,255);
-  }
+void display () {
+  u8g2.firstPage();
+    do {
+      u8g2.setFont(u8g2_font_ncenB10_tr);
+      u8g2.drawStr(0,24,"Hello World!");
+    } while ( u8g2.nextPage() );
+  delay(1000);
 }
 
-void loopDisplay () {
-  u8g.firstPage();  
-  do {
-    draw();
-  } while( u8g.nextPage() ); 
-}
-
-void draw () {
-  u8g.setFont(u8g_font_unifont);
-  u8g.drawStr( 0, 22, chronoValsToPrint);
-}
 
 void chrono () {
-    if (hasFirstTripped == false && (map(analogRead(IR_REC_ONE_PIN), 0, 1024, 0, 100) > IR_GATE_TRIP_VAL) ) {
-        firstTripTime = micros();
-        hasFirstTripped = true;
-    }
-    if (hasFirstTripped == true && (map(analogRead(IR_REC_TWO_PIN), 0, 1024, 0, 100) > IR_GATE_TRIP_VAL) ) {
-        secondTripTime = micros();
-        hasFirstTripped = false;
-        chronoValsToPrint = (String)calculateChronoReadings();
-    }
+  if (hasFirstTripped == false && (map(analogRead(IR_REC_ONE_PIN), 0, 1024, 0, 100) > IR_GATE_TRIP_VAL) ) {
+    firstTripTime = micros();
+    hasFirstTripped = true;
+  }
+  if (hasFirstTripped == true && (map(analogRead(IR_REC_TWO_PIN), 0, 1024, 0, 100) > IR_GATE_TRIP_VAL) ) {
+    secondTripTime = micros();
+    hasFirstTripped = false;
+    chronoValsToPrint = (String)calculateChronoReadings();
+  }
 }
 
 double calculateChronoReadings () {
-    double timeElapsed = (secondTripTime - firstTripTime) * 1000000;
-    double displacement = GATE_DISPLACEMENT / 12;
+  double timeElapsed = (secondTripTime - firstTripTime) * 1000000;
+  double displacement = GATE_DISPLACEMENT / 12;
 
-    return displacement/timeElapsed;
+  return displacement/timeElapsed;
 }
 
 
